ffbaf9992ae3903fb325a681e7d3fea25d23e4a5
Amari James <mar.james1998@gmail.com>
2021-02-05 10:03:24 -0500

    secondschange?

--- app.js
+++ app.js
@@ -56,7 +56,6 @@ const extendTimeoutMiddleware = (req, res, next) => {
   let isFinished = false;
   let isDataSent = false;
 
-  // Only extend the timeout for API requests
   if (!req.url.includes('/api')) {
     next();
     return;
@@ -75,8 +74,7 @@ const extendTimeoutMiddleware = (req, res, next) => {
   });
 
   res.on('data', (data) => {
-    // Look for something other than our blank space to indicate that real
-    // data is now being sent back to the client.
+
     if (data !== space) {
       isDataSent = true;
     }
@@ -84,19 +82,17 @@ const extendTimeoutMiddleware = (req, res, next) => {
 
   const waitAndSend = () => {
     setTimeout(() => {
-      // If the response hasn't finished and hasn't sent any data back....
       if (!isFinished && !isDataSent) {
-        // Need to write the status code/headers if they haven't been sent yet.
+
         if (!res.headersSent) {
           res.writeHead(202);
         }
 
         res.write(space);
 
-        // Wait another 15 seconds
         waitAndSend();
       }
-    }, 15000);
+    }, 55000);
   };
 
   waitAndSend();
