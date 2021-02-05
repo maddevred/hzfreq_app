a2cd30b72e1b09d509c12592d2404c3e110b2c9f
Amari James <mar.james1998@gmail.com>
2021-02-04 21:49:55 -0500

    idkman

--- app.js
+++ app.js
@@ -49,29 +49,6 @@ app.use(fileUpload({
     limits: { fileSize: 50 * 1024 * 1024 },
 }));
 
-var bodyParser = require('body-parser')
-var connect = require('connect')
-var timeout = require('connect-timeout')
-
-app.use('/save', timeout('5s'), bodyParser.json(), haltOnTimedout, function (req, res, next) {
-  savePost(req.body, function (err, id) {
-    if (err) return next(err)
-    if (req.timedout) return
-    res.send('saved as id ' + id)
-  })
-})
-
-function haltOnTimedout (req, res, next) {
-  if (!req.timedout) next()
-}
-
-function savePost (post, cb) {
-  setTimeout(function () {
-    cb(null, ((Math.random() * 40000) >>> 0))
-  }, (Math.random() * 7000) >>> 0)
-}
-
-
 // app.listen(3000); 
 
 module.exports = app;
